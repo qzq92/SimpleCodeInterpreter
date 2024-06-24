c6d1c9e66cd985b85336bdfeb4c0ed54e42fde29
Zhi Qiang QUEK <zhiqiang.quek@biposervice.com>
2024-06-24 14:32:11 +0800

    Moved prompt templates and declarations for python/csv agent to agents subfolder

--- main.py
+++ main.py
@@ -1,59 +1,76 @@
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
 from langchain.agents import create_react_agent, AgentExecutor
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
 from langchain_experimental.tools import PythonREPLTool
+from langchain.tools import Tool
 from textwrap import dedent
 from langchain import hub
+from typing import Any
+from agents.agent import get_python_agent_executor, get_csv_agent_executor
 import os
 
 load_dotenv()
 
 def main():
-    print("Start...")
 
-    prompt_instructions = dedent("""\
-        You are an agent designed to always write and execute python code to answer questions. Answer should be based on python code output. You have access to python REPL for executing python code. Should you get an error, perform code debugging and try again. If you are unable to write code to answer the question, return "I do not know" as the answer."""
-        )
+    csv_agent_executor = get_csv_agent_executor()
+
+
+    def python_agent_executor_wrapper(original_prompt: str) -> dict[str, Any]:
+        """Wrapper function take takes in a string prompt and calls an AgentExecutor's invoke method with arbitrary dictionary containing the input string passed to 'input' key. 
+
+        Args:
+            original_prompt (str): Prompt in string type.
+
+        Returns:
+            dict[str, Any]: Agent Executor response.
+        """
+        python_agent_executor = get_python_agent_executor()
+
+        return python_agent_executor.invoke({"input": original_prompt})
+    
+    router_agent_tools_list = [
+        Tool(
+            name = "Python Agent",
+            func = python_agent_executor_wrapper,
+            description = """Useful when you need to transform natural language to Python, execute the Python code and return the results from code execution. DOES NOT ACCPEPT CODE AS INPUT""",
+        ),
+        Tool(
+            name = "CSV Agent",
+            func = csv_agent_executor.invoke,
+            description = """Useful when you need to answer question over a .csv file, takes an input the entire question and returns the answer after running Python's pandas calculations""",
+        ),
+    ]
 
     base_prompt = hub.pull(os.environ.get("HUB_TEMPLATE"))
-    react_prompt = base_prompt.partial(instructions=prompt_instructions)
-    
-    llm = ChatOpenAI(temperature=0, model=os.environ.get("OPENAI_MODEL"))
+    # No instruction is to be fed to router agent
+    prompt = base_prompt.partial(instructions="")
 
-    # Define tool list, containing tools to use, should be Tool object with docstring. Custom function can be converted to a tool with @tool decorator.
-    tools_lists = [PythonREPLTool()]
-
-    # Create agent as recipe with react prompt to reason and act based on outputs obtained from tools used.
-    qr_agent = create_react_agent(
-        llm=llm,
-        tools=tools_lists,
-        prompt=react_prompt
+    # Instantiate react agent
+    router_agent = create_react_agent(
+        prompt = prompt,
+        llm = ChatOpenAI(temperature=0,
+                model=os.environ.get("OPENAI_MODEL")
+            ),
+        tools = router_agent_tools_list,
     )
 
-    # Orchestrator for agent with verbose enabled to track its inner workings
-    qr_agent_executor = AgentExecutor(
-        agent=qr_agent,
-        tools=tools_lists,
+    router_agent_executor = AgentExecutor(
+        agent=router_agent,
+        tools=router_agent_tools_list,
         verbose=True
     )
 
-    input_prompt = dedent(
-        """\
-            Generate and save in current working directory, under a folder 'qrcodes' the following:\
-                  1 QR codes pointing to https://www.channelnewsasia.com with the filename labelled as 'cna.png'; and 1 QR code pointing to https://www.straitstimes.com/ with the filename labelled as 'straitstimes.png'. You have 'qrcode' package installed and available for use.
-        """)
+    # Test prompt #1
+    prompt_1 = "Which season has the most episodes?"
+    print(router_agent_executor.invoke({"input": prompt_1}))
+
+    prompt_2 = "Generate and save in current working directory, under a folder 'qrcodes' the following:\n\n 1 QR code pointing to provided url: https://www.techtarget.com/ and save as techtarget.png "
     
-    # Invoke agent for generating QR code
-    qr_agent_executor.invoke({input:input_prompt})
+    print(router_agent_executor.invoke({"input": prompt_2}))
 
-
-    # CSV agent declaration and instantiation. Due to it being an experimental stuff
-    # csv_agent = create_csv_agent(
-    #     llm=llm,
-    #     path=os.environ.get("CSV_FILEPATH"),
-    #     verbose=True
-    # )
-
+# Main function
 if __name__ == "__main__":
     main()
